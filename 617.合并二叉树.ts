e7ea0b20292a3b79f525d45133b3ddfa4bea9d9e
梁家辉 <1360542446@qq.com>
2020-07-18 15:34:02 +0800

    feat: 617

--- 617.合并二叉树.ts
+++ 617.合并二叉树.ts
@@ -20,7 +20,26 @@
  */
 
 function mergeTrees(t1: TreeNode | null, t2: TreeNode | null): TreeNode | null {
+  if (!t1 && !t2) return null;
+  if (t1 && t2) {
+    t1.val += t2.val;
+    t1.left = mergeTrees(t1.left, t2.left);
+    t1.right = mergeTrees(t1.right, t2.right);
+  } else {
+    return t1 || t2;
+  }
 
-};
+  return t1;
+}
 // @lc code=end
+// [1, 3, 2, 5][(2, 1, 3, null, 4, null, 7)];
+//
 
+// if (!t1) return t2;
+// if (!t2) return t1;
+
+// t1.val += t2.val;
+// t1.left = mergeTrees(t1.left, t2.left);
+// t1.right = mergeTrees(t1.right, t2.right);
+
+// return t1;
